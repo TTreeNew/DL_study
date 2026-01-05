d6fe485abbe659abdda3127e693977bda1399b28
TTreeNew <swang2792@gmail.com>
2026-01-05 17:46:20 +0800

    完成

--- lesson7/two_import_model_test.py
+++ lesson7/two_import_model_test.py
@@ -2,6 +2,9 @@ import dataset
 import numpy as np
 import matplotlib.pyplot as plt
 
+# # 启用交互模式 
+# plt.ion() 
+
 xs,ys,zs = dataset.get_beans("dataset_5.pt")
 x_axis = np.linspace(0, 1, 30)  
 y_axis = np.linspace(0, 1, 30)
@@ -33,11 +36,12 @@ def forward_propgation(x1,x2):
 a,z = forward_propgation(x_axis,y_axis)
 ax.plot_surface(x_axis, y_axis, z, alpha=0.5, color='orange') #画曲面
 plt.show()  
+# plt.show(block=False)  # 使用 block=False 避免阻塞
+# plt.pause(0.1) 
 
-
-# 创建新的图形窗口用于训练
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+# # 创建新的图形窗口用于训练
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
 
 for epoch in range(1000):
     for i in range(200):
@@ -67,18 +71,27 @@ for epoch in range(1000):
         w2= w2 - alpha*dedw2
         b =b - alpha*dedb
 
+
     if epoch % 10 == 0 :
         a,z = forward_propgation(x_axis,y_axis)
         ax.cla()# 清除轴并重新绘制
-        ax.set_xlim(0,1)
-        ax.set_ylim(-0.2,1.2)
-        ax.set_zlim(-0.2,1.2)
-        ax.set_xlabel('Bean Size')
-        ax.set_ylabel('Color depth')
-        ax.set_zlabel('Toxicity')
-        
+
         # 绘制数据点和曲面
         ax.scatter(xs, ys, zs)
         ax.plot_surface(x_axis,y_axis,a) #这里xs,ys,a都是网格（矩阵），用来画曲面。如果是向量画的就是空间曲线
         # plt.draw()  
         plt.pause(0.01) 
+
+
+        # ax.collections.clear()#保留窗口清除曲面
+        # ax.scatter(xs,ys,zs)
+        # a,z = forward_propgation(x_axis,y_axis)
+        # # ax.set_xlim(0,1)
+        # # ax.set_ylim(-0.2,1.2)
+        # # ax.set_zlim(-0.2,1.2)
+        # # ax.set_xlabel('Bean Size')
+        # # ax.set_ylabel('Color depth')
+        # # ax.set_zlabel('Toxicity')
+        # ax.plot_surface(x_axis,y_axis,a) #这里xs,ys,a都是网格（矩阵），用来画曲面。如果是向量画的就是空间曲线
+        # fig.canvas.draw()
+        # plt.pause(0.001)#暂停0.001s
